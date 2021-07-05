e0214240bb9983ca90118a879d47007805b825ed
Shinya Sasaki <shinya.sasaki@kek.jp>
2021-07-05 15:48:03 +0900

    Update alarm-config to use subgroups

--- generate.py
+++ generate.py
@@ -11,9 +11,21 @@ def parseArgs():
     parser.add_argument('-r', '-rate', dest='rate',
                         default='.2 second', help='Ramp Scan Rate',
                         metavar='.2 second')
-    parser.add_argument('-n', '-number', dest='number',
-                        default=200, help='Number of Count for Alarm',
-                        metavar=200, type=int)
+    parser.add_argument('-a', '-area', dest='area',
+                        default=5, help='Number of Areas',
+                        metavar=5, type=int)
+    parser.add_argument('-s', '-subgroup', dest='sgroup',
+                        default=10, help='Number of Sub Groups',
+                        metavar=10, type=int)
+    parser.add_argument('-u', '-subsubgroup', dest='ssgroup',
+                        default=10, help='Number of Sub Sub Groups',
+                        metavar=10, type=int)
+    parser.add_argument('-d', '-dbgroup', dest='dbgroup',
+                        default=250, help='Number of Groups for Alarm Record DB',
+                        metavar=250, type=int)
+    parser.add_argument('-c', '-count', dest='count',
+                        default=50000, help='Number of Count for Ramp Record',
+                        metavar=50000, type=int)
     parser.add_argument('-o', '-out', dest='out',
                         default='./alarm.db', help='Output Path for DB',
                         metavar='./alarm.db')
@@ -30,7 +42,7 @@ def checkdir(path):
     if not parent.exists():
         parent.mkdir()
 
-def generate(total, group, rate, count, dbpath, confpath):
+def generate(total, area, sgroup, ssgroup, agroup, rate, count, dbpath, confpath):
     dpath = pathlib.Path(dbpath)
     cpath = pathlib.Path(confpath)
 
@@ -40,14 +52,14 @@ def generate(total, group, rate, count, dbpath, confpath):
     env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
     tmpl_db = env.get_template('db.template')
     
-    db = tmpl_db.render(total=total, group=group, rate=rate, count=count)
+    db = tmpl_db.render(total=total, group=agroup, rate=rate, count=count)
     
     with open(dpath, 'w') as f:
         f.write(db)
     
     tmpl_config = env.get_template('alarm-config.template')
     
-    config = tmpl_config.render(total=total)
+    config = tmpl_config.render(total=total, area=area, sgroup=sgroup, ssgroup=ssgroup)
     
     with open(cpath, 'w') as f:
         f.write(config)
@@ -56,12 +68,16 @@ def main():
     args = parseArgs()
 
     total = args.total
-    n = args.number
+    area = args.area
+    sgroup = args.sgroup
+    ssgroup = args.ssgroup
+    dbgroup = args.dbgroup
     rate = args.rate
     dbpath = args.out
+    count = args.count
     confpath = args.xml
 
-    generate(total, n, rate, 100, dbpath, confpath)
+    generate(total, area, sgroup, ssgroup, dbgroup, rate, count, dbpath, confpath)
     
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def test_alarm_files():
     test_dpath = './tests/out/alarm.db'
     test_cpath = './tests/out/alarm-config.xml'
 
-    generate.generate(total=2, group=2, rate='.2 second', count=100, dbpath=test_dpath, confpath=test_cpath)
+    generate.generate(total=16, area=2, sgroup=2, ssgroup=2, agroup=4, rate='.2 second', count=100, dbpath=test_dpath, confpath=test_cpath)
 
     assert filecmp.cmp('./tests/alarm-config.xml', test_cpath)
     assert filecmp.cmp('./tests/alarm.db', test_dpath)
